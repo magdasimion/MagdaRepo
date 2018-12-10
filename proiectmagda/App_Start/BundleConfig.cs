242fb53bb661f9e395d4172d13e75f448c22d968
Magda Simion <45066279+magdasimion@users.noreply.github.com>
2018-12-10 15:43:11 +0200

    mesaj

--- proiectmagda/App_Start/BundleConfig.cs
+++ proiectmagda/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace proiectmagda
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                     "~/Content/fontawsome.css",
                       "~/Content/site.css"));
         }
     }
---
+++ proiectmagda/google/maps.cs
@@ -0,0 +1,17 @@
+﻿namespace google
+{
+    internal class maps
+    {
+        internal class Map
+        {
+            private object getElementById;
+            private string v;
+
+            public Map(object getElementById, string v)
+            {
+                this.getElementById = getElementById;
+                this.v = v;
+            }
+        }
+    }
+}
